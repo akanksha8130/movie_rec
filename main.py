04de36a9c1dbb3a47e6710e2c73876321e6b5b10
akanksha8130 <71652318+akanksha8130@users.noreply.github.com>
2024-12-02 15:15:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request
+
+from storage import all_movies, liked_movies, not_liked_movies, did_not_watch
+from demographic_filtering import output
+from content_filtering import get_recommendations
+
+app = Flask(__name__)
+
+@app.route("/get-movie")
+def get_movie():
+    movie_data = {
+        "title": all_movies[0][19],
+        "poster_link": all_movies[0][27],
+        "release_date": all_movies[0][13] or "N/A",
+        "duration": all_movies[0][15],
+        "rating": all_movies[0][20],
+        "overview": all_movies[0][9]
+    }
+    return jsonify({
+        "data": movie_data,
+        "status": "success"
+    })
+
+@app.route("/liked-movie", methods=["POST"])
+def liked_movie():
+    movie = all_movies[0]
+    liked_movies.append(movie)
+    all_movies.pop(0)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+@app.route("/unliked-movie", methods=["POST"])
+def unliked_movie():
+    movie = all_movies[0]
+    not_liked_movies.append(movie)
+    all_movies.pop(0)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+@app.route("/did-not-watch", methods=["POST"])
+def did_not_watch_view():
+    movie = all_movies[0]
+    did_not_watch.append(movie)
+    all_movies.pop(0)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+
+if __name__ == "__main__":
+  app.run()
